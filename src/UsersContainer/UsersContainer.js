9af343d60b5d0adba9db268de3d79a89de290e69
Wally Wallace <jrvw001@gmail.com>
2025-01-22 11:32:20 -0700

    Add skeleton users container

---
+++ src/UsersContainer/UsersContainer.js
@@ -0,0 +1,9 @@
+import UserCard from '../UserCard/UserCard';
+import './UserContainer.css';
+import React, { useState, useEffect } from "react";
+
+function UserCard(){
+
+}
+
+export default UserCard;
